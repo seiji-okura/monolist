d25fa857e47a759c013a9ac73a225e16dc29c12b
seiji_okura <slower.than.snails@gmail.com>
2016-03-01 08:11:46 +0000

    ranking review03

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,10 +1,14 @@
 class RankingController < ApplicationController
 
   def have
-    @ranking = Ownership.where(type: 'Have').group(:item_id).limit(10).order("count_item_id desc").count(:item_id)
+    @counts = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    ids = @counts.keys
+    @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
   end
 
   def want
-    @ranking = Ownership.where(type: 'Want').group(:item_id).limit(10).order("count_item_id desc").count(:item_id)
+    @counts = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id)
+    ids = @counts.keys
+    @items = Item.find(ids).sort_by{|o| ids.index(o.id)}
   end
 end
